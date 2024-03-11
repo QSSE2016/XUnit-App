83aeba6441869d42de827a2d33a7dad0a0bbe7da
Alexandros <alexstathas1996@gmail.com>
2024-03-11 16:58:54 +0200

    Added a calculator class to test.

---
+++ XTA/XTA/Calculator.cs
@@ -0,0 +1,13 @@
+﻿namespace XTA
+{
+    public static class Calculator
+    {
+        public static double Add(double a, double b) { return  a + b; }
+        public static double Subtract(double a, double b) { return a - b; }
+        public static double Multiply(double a, double b) { return a * b; }
+        public static double Divide(double a, double b) {
+            if(b == 0) return a;
+            return a / b; 
+        }
+    }
+}
