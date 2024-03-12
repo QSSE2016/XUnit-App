acf7fd18a0e4986f04c732a18a20aa9665078722
Alexandros <alexstathas1996@gmail.com>
2024-03-12 19:00:09 +0200

    Added tests for divide method.

--- XTA/XTA.Tests/CalculatorTest.cs
+++ XTA/XTA.Tests/CalculatorTest.cs
@@ -28,5 +28,19 @@ namespace XTA.Tests
             var actual = Calculator.Subtract(a, b);
             Assert.Equal(expected, actual, 0); // 3rd value is tolerance meaning difference.
         }
+
+        // Here we have a "theory" aka a way to do tests with parameters. Each InlineData defines values matching the arguments of the function and essentially
+        // defines a single test. There are others too (ClassData,PropertyData), but all they do is just make it so you can put values inside of a class or property, nothing else.
+
+        [Theory(DisplayName = "A divided by B. In the case that B is zero, we should see A being returned.")]
+        [InlineData(1,1,1)]
+        [InlineData(2,0,2)]
+        [InlineData(10,5,2)]
+        public void Divide(double a,double b, double expected)
+        {
+            var value = Calculator.Divide(a, b);
+            Assert.Equal(expected, value,0);
+        }
+
     }
 }
